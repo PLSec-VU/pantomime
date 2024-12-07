aade2b3de53261120e414399a1668b4009947fbc
Klaus Gleissenthall <klausgleissenthall@mac-1.home>
2024-12-07 21:44:54 +0100

    processor

--- app/TwoAddersStallPrecise.hs
+++ app/TwoAddersStallPrecise.hs
@@ -9,11 +9,11 @@ module TwoAddersStallPrecise
   , sim2
   , proj2
   , add2
-  , test
-  , add12
-  , obs12
-  , sim12
-  , proj12
+  --, test
+--   , add12
+--   , obs12
+--   , sim12
+--   , proj12
   ) where
 
 import Test.QuickCheck
@@ -35,28 +35,28 @@ add1 reg (stall, Just imm)
     | otherwise = (reg + imm, Just (reg + imm))
 add1 reg (_, Nothing) = (reg, Nothing) 
 
-leak1 :: () -> (Bool, Maybe Bool) -> ((), (Bool, Maybe Bool))
-leak1 _ (stall, res) = ((), (stall, res))
+leak1 :: Int -> (Bool, Maybe Int) -> (Int, Maybe Bool)
+leak1 reg (stall, Just imm)  
+    | stall     = (reg, Nothing)     
+    | otherwise = (reg + imm, Just (reg + imm==0))
+leak1 reg (_, Nothing) = (reg, Nothing) 
 
 obs1 :: Maybe Int -> Maybe Bool
 obs1 (Just r) = Just (r==0)
 obs1 Nothing  = Nothing
 
-sim1 :: () -> (Bool, Maybe Bool) -> ((), Maybe Bool)
-sim1 _ (stall, Just res)
-    | stall     = ((), Nothing)     
-    | otherwise = ((), Just res)
-sim1 _ (_, Nothing) = ((), Nothing) 
+sim1 :: () -> Maybe Bool -> ((), Maybe Bool)
+sim1 _ o1 = ((), o1)
 
-proj1 :: Int -> ((), ())
-proj1 reg = ((), ())
+proj1 :: Int -> (Int, ())
+proj1 reg = (reg, ())
 
--- {-# ANN add2 UC
---   { observation = 'obs2
---   , leakage = 'leak2
---   , simulator = 'sim2
---   , projection = 'proj2
---   } #-}
+{-# ANN add2 UC
+  { observation = 'obs2
+  , leakage = 'leak2
+  , simulator = 'sim2
+  , projection = 'proj2
+  } #-}
 
 isJust :: Maybe a -> Bool
 isJust (Just _) = True
@@ -74,8 +74,10 @@ obs2 :: (Maybe Int, Bool) -> (Bool, Bool)
 obs2 (Just _, stall) = (True, stall)
 obs2 (Nothing, stall) = (False, stall)
 
-leak2 :: () -> (Maybe Bool , Bool) -> ((), (Maybe Bool, Bool))
-leak2 _ (l1, l2) = ((), (l1,  l2))
+leak2 :: () -> (Maybe Int, Maybe Int) -> ((), (Maybe Bool, Bool))
+leak2 _ (Just res, i2) = ((), (Just $ res == 0,  isJust i2))
+leak2 _ (Nothing, i2) = ((), (Nothing,  isJust i2))
+
 
 sim2 :: Bool -> (Maybe Bool , Bool) -> (Bool , (Bool, Bool))
 sim2 _ (Just True, True) = (False, (True, False))
@@ -108,55 +110,53 @@ obs12 = isJust
 
 -- | leak12 needs to keep track of the accurate register state of add1 (to leak when it will be 0).
 -- | for that, it needs to know when the overall circuit will stall.
-leak12 :: (Int, Bool) -> (Maybe Int, Maybe Int) -> ((Int, Bool), (Maybe Bool, Bool))
-leak12 (reg, stall) (i1, i2) = case (stall, i1) of
-    (True, _)         -> ((reg, False), (Nothing, isJust i2))
-    (False, Just imm) -> ((reg + imm, i2 /= Nothing && reg + imm/=0), (Just (reg + imm==0), isJust i2))
-    (False, Nothing)  -> ((reg, False), (Nothing, isJust i2))
 
 -- leak12 :: (Int, Bool) -> (Maybe Int, Maybe Int) -> ((Int, Bool), (Maybe Bool, Bool))
--- leak12 (reg, stall) (Just imm, Just _) 
---     | stall     = ((reg, False), (Nothing, True))     
---     | otherwise = ((reg + imm, reg + imm/=0), (Just (reg + imm==0), True))
--- leak12 (reg, _) (Nothing, i2) = ((reg, False), (Nothing, isJust i2)) 
--- leak12 (reg, stall) (Just imm, Nothing) 
---     | stall     = ((reg, False), (Nothing, False))     
---     | otherwise = ((reg + imm, False), (Just (reg + imm==0), False))
+-- leak12 (reg, stall) (i1, i2) = case (stall, i1) of
+--     (True, _)         -> ((reg, False), (Nothing, isJust i2))
+--     (False, Just imm) -> ((reg + imm, i2 /= Nothing && reg + imm/=0), (Just (reg + imm==0), isJust i2))
+--     (False, Nothing)  -> ((reg, False), (Nothing, isJust i2))
+
+-- leak12 :: (Int, Bool) -> (Maybe Int, Maybe Int) -> ((Int, Bool), (Maybe Bool, Bool))
+-- leak12 (reg, stall) (i1, i2) = case (stall, i1) of
+--     (True, _)         -> ((reg, False), (Nothing, isJust i2))
+--     (False, Just imm) -> ((reg + imm, i2 /= Nothing && reg + imm/=0), (Just (reg + imm==0), isJust i2))
+--     (False, Nothing)  -> ((reg, False), (Nothing, isJust i2))
 
 
-sim12 :: ((), Bool, Bool)  -> (Maybe Bool, Bool) -> (((), Bool, Bool), Bool)
-sim12 (s1, s2, stalled) (l1, l2) = ((s1', s2', stall), o2)     
-    where
-    (s1', o1) = sim1 s1 (stalled, l1)
-    (s2', (o2, stall)) = sim2 s2 (o1, l2)
+-- sim12 :: ((), Bool, Bool)  -> (Maybe Bool, Bool) -> (((), Bool, Bool), Bool)
+-- sim12 (s1, s2, stalled) (l1, l2) = ((s1', s2', stall), o2)     
+--     where
+--     (s1', o1) = sim1 s1 (stalled, l1)
+--     (s2', (o2, stall)) = sim2 s2 (o1, l2)
 
-proj12 :: (Int, Maybe Int, Bool) -> ((), (Int, Bool, Bool))
-proj12 (s1, s2, stalled) = ((), (s1, isJust s2, stalled))
+-- proj12 :: (Int, Maybe Int, Bool) -> ((Int, Bool), ((), Bool, Bool))
+-- proj12 (s1, s2, stalled) = ((s1, stalled), ((), isJust s2, stalled))
 
--- ----------------------------------------------------------------------
--- | Test corner -- we can check our theorems using quick-check
--------------------------------------------------------------------------
-mealy :: (s -> i -> (s, o)) -> s -> [i] -> [o]
-mealy _ _ [] = []
-mealy f s (x:xs) =
-    let (s', o) = f s x
-    in o : mealy f s' xs
+-- -- ----------------------------------------------------------------------
+-- -- | Test corner -- we can check our theorems using quick-check
+-- -------------------------------------------------------------------------
+-- mealy :: (s -> i -> (s, o)) -> s -> [i] -> [o]
+-- mealy _ _ [] = []
+-- mealy f s (x:xs) =
+--     let (s', o) = f s x
+--     in o : mealy f s' xs
 
--- | alps * operator  
-seqc :: (s1 -> i1 -> (s1, o)) -> (s2 -> o -> (s2, o2)) -> (s1,s2) -> i1 -> ((s1,s2), o2)
-seqc c1 c2 (s1,s2) i = ((s1',s2'), o2)
-    where
-      (s1', o1) = c1 s1 i
-      (s2', o2) = c2 s2 o1 
+-- -- | alps * operator  
+-- seqc :: (s1 -> i1 -> (s1, o)) -> (s2 -> o -> (s2, o2)) -> (s1,s2) -> i1 -> ((s1,s2), o2)
+-- seqc c1 c2 (s1,s2) i = ((s1',s2'), o2)
+--     where
+--       (s1', o1) = c1 s1 i
+--       (s2', o2) = c2 s2 o1 
 
-theorem :: [(Maybe Int, Maybe Int)] -> Bool
-theorem ins = mealy add12_obs (0, Nothing, False) ins == mealy leak12_sim ((0,False),((), False, False)) ins
-    where 
-        add12_obs s i =  let (s', o) = add12 s i in (s', obs12 o)
-        leak12_sim = seqc leak12 sim12
+-- theorem :: [(Maybe Int, Maybe Int)] -> Bool
+-- theorem ins = mealy add12_obs (0, Nothing, False) ins == mealy leak12_sim ((0,False),((), False, False)) ins
+--     where 
+--         add12_obs s i =  let (s', o) = add12 s i in (s', obs12 o)
+--         leak12_sim = seqc leak12 sim12
 
-test = do
-    quickCheckWith stdArgs { maxSuccess = 1000000 } theorem
+-- test = do
+--     quickCheckWith stdArgs { maxSuccess = 1000000 } theorem
 --     let ins = [(Just 1,Nothing),(Just 0,Just 0),(Nothing,Nothing)]
 --     putStrLn "Implementation"
 --     putStrLn $ show $ mealy add12_obs (0, Nothing, False) ins
