8c1acb0ea7510e74f2483480291d70a4b2eceb52
Wind <im@windtfw.com>
2026-05-30 22:32:28 +0200

    Fix compilation on CI

--- test/Spec.hs
+++ test/Spec.hs
@@ -6,15 +6,13 @@ import Test.Hspec
 import Test.HUnit
 
 import System.Directory (listDirectory)
-import System.IO.Silently (hSilence)
-import System.IO (stdout, stderr)
 
 import GHC
 import GHC.Paths (libdir)
 import GHC.Plugins (HasDynFlags (..), showGhcExceptionUnsafe)
 -- import GHC.Utils.Panic (handleGhcException)
 import GHC.Driver.Session (updOptLevel)
-import qualified GHC.Data.EnumSet as EnumSet
+import GHC.Data.EnumSet qualified as EnumSet
 
 import Language.Haskell.TH.LanguageExtensions
 
@@ -30,7 +28,7 @@ setupPlugin :: Ghc ()
 setupPlugin = do
   dflags <- getDynFlags
   setSessionDynFlags $ updOptLevel 1 dflags
-    { pluginModNames = mkModuleName "UC" : pluginModNames dflags
+    { pluginModNames = mkModuleName "Pantomime" : pluginModNames dflags
     , extensionFlags = extensionFlags dflags 
       & EnumSet.insert TemplateHaskellQuotes
     , generalFlags = generalFlags dflags
@@ -68,8 +66,8 @@ progPaths = do
   let listDirectory' dir = do
         files <- listDirectory dir
         pure $ fmap (dir <>) files
-  paths <- listDirectory' "prog/"
-  let isSrc = isSuffixOf ".hs"
+  paths <- listDirectory' "tests/"
+  let isSrc path = isSuffixOf ".hs" path && not ("ForceEmbedding.hs" `isSuffixOf` path)
   pure $ filter isSrc paths
 
 -- -- | Compiles the given files, running them through the plugin.
